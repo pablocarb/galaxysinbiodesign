5769bed91f865a97917cda6051ae7008b762310b
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2019-04-26 16:01:27 +0100

    python compatibility

--- maketool.sh
+++ maketool.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # author: Pablo Carbonell
 # Create Selenzyme Galaxy tool using planemo.
+#    --example_input reaction.csv \
+#    --example_output out.csv \
 
 planemo tool_init --force \
     --id 'selenzyme' \
     --name 'Query Selenzyme: Enzyme sequence selection' \
-    --requirement requests \
-    --example_command 'python toolSelenzyme.py $input $output' \
-    --example_input reaction.csv \
-    --example_output out.csv \
+    --requirement requests@2 \
+    --requirement python@3 \
+    --command 'python3 toolSelenzyme.py $input $output' \
     --doi 10.1093/bioinformatics/bty065 \
-    --help_from_command 'python toolSelenzyme.py -h'
+    --help_text 'This tool calls Selenzyme to select enzyme sequences for a given target reaction represented in SMARTS.'
     
