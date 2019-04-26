2a9bfdded59c7821f030850cdb4c97ce631959c6
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2019-04-26 16:04:28 +0100

    Working on template

--- maketool.sh
+++ maketool.sh
@@ -9,7 +9,9 @@ planemo tool_init --force \
     --name 'Query Selenzyme: Enzyme sequence selection' \
     --requirement requests@2 \
     --requirement python@3 \
-    --command 'python3 toolSelenzyme.py $input $output' \
+    --example_command 'python3 toolSelenzyme.py $input $output' \
+    --example_input 'reaction.csv' \
+    --example_output 'out.csv'
     --doi 10.1093/bioinformatics/bty065 \
     --help_text 'This tool calls Selenzyme to select enzyme sequences for a given target reaction represented in SMARTS.'
     
