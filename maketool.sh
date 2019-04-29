1a974ea46c4c60cc5419aea6a9fc8d713bc1435c
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2019-04-29 11:55:32 +0100

    Additional params

--- maketool.sh
+++ maketool.sh
@@ -8,9 +8,15 @@ planemo tool_init --force \
     --id 'selenzyme' \
     --name 'Query Selenzyme: Enzyme sequence selection' \
     --requirement requests@2 \
-    --example_command 'python $__tool_directory__/toolSelenzyme.py $input1 $output1' \
+    --example_command 'python $__tool_directory__/toolSelenzyme.py $input1 $output1 -server $server' \
     --example_input 'reaction.csv' \
     --example_output 'out.csv' \
     --doi 10.1093/bioinformatics/bty065 \
     --help_from_command 'python3 toolSelenzyme.py -h'
     
+planemo shed_init . --force \
+    --name selenzyme \
+    --owner pablocarb \
+    --description 'Selenzyme tool: Enzyme sequence selection from reaction rules' \
+    --category 'Systems Biology' \
+    --homepage_url 'https://github.com/pablocarb/selenzyme_tool'
