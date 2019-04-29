d66a361b807a9a9f970e24b8964e534ff201fa17
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2019-04-29 15:56:45 +0100

    Working version, installed in the toolshed

--- maketool.sh
+++ maketool.sh
@@ -5,20 +5,33 @@
 #    --example_output out.csv \
 #    --example_command 'python $__tool_directory__/toolSelenzyme.py $input1 $output1 -server $server' \
 
+# Generate tool backbone (additional params need to be entered manually 
 planemo tool_init --force \
     --id 'selenzyme' \
     --name 'Selenzyme' \
     --description 'enzyme sequence selection from reaction template' \
     --requirement requests@2 \
-    --example_command 'python $__tool_directory__/toolSelenzyme.py $input1 $output1' \
+    --example_command 'python $__tool_directory__/toolSelenzyme.py $input1 $output1 -server $server' \
     --example_input 'reaction.csv' \
     --example_output 'out.csv' \
     --doi 10.1093/bioinformatics/bty065 \
     --help_from_command 'python3 toolSelenzyme.py -h'
-    
+   
+# Init shed repository
 planemo shed_init . --force \
-    --name selenzyme \
+    --name synbiodesign \
     --owner pablocarb \
-    --description 'Selenzyme tool: Enzyme sequence selection from reaction rules' \
+    --description 'BioCAD for Industrial Biotechnology' \
     --category 'Systems Biology' \
-    --homepage_url 'https://github.com/pablocarb/selenzyme_tool'
+    --homepage_url 'https://github.com/pablocarb/galaxysynbiodesign'
+
+# Create repository in the toolshed
+planemo shed_create \
+    --shed_target toolshed \
+    --shed_key_from_env TOOLSHED 
+
+# Update repository in the toolshed
+planemo shed_update \
+    --check_diff --shed_target toolshed \
+    --shed_key_from_env TOOLSHED
+
